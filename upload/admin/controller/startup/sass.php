dde718caf22d0bd31e3c5a174bbcf40a9854376d
condor2 <condor20_05@yahoo.it>
2022-04-08 12:53:03 +0300

    Wrong SASS code

--- upload/admin/controller/startup/sass.php
+++ upload/admin/controller/startup/sass.php
@@ -1,34 +1,25 @@
 <?php
 class ControllerStartupSass extends Controller {
 	public function index() {
-		$files = glob(DIR_APPLICATION . 'view/stylesheet/*.scss');
+		$file = DIR_APPLICATION . 'view/stylesheet/bootstrap.css';
 
-		if ($files) {
-			foreach ($files as $file) {
-				// Get the filename
-				$filename = basename($file, '.scss');
+		if (!is_file($file)) {
+			$scss = new \ScssPhp\ScssPhp\Compiler();
+			$scss->setImportPaths(DIR_APPLICATION . 'view/stylesheet/sass/');
 
-				$stylesheet = DIR_APPLICATION . 'view/stylesheet/' . $filename . '.css';
+			$output = $scss->compileString('@import "_bootstrap.scss"')->getCss();
 
-				if (!is_file($stylesheet) || !$this->config->get('developer_sass')) {
-					$scss = new \ScssPhp\ScssPhp\Compiler();
-					$scss->setImportPaths(DIR_APPLICATION . 'view/stylesheet/');
+			$handle = fopen($file, 'w');
 
-					$output = $scss->compileString('@import "' . $filename . '.scss"')->getCss();
+			flock($handle, LOCK_EX);
 
-					$handle = fopen($stylesheet, 'w');
+			fwrite($handle, $output);
 
-					flock($handle, LOCK_EX);
+			fflush($handle);
 
-					fwrite($handle, $output);
+			flock($handle, LOCK_UN);
 
-					fflush($handle);
-
-					flock($handle, LOCK_UN);
-
-					fclose($handle);
-				}
-			}
+			fclose($handle);
 		}
 	}
 }
