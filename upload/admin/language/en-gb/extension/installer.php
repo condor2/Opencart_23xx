76cbf680619baf2165195ef7aa4ea0d75cc3ea60
condor2 <condor20_05@yahoo.it>
2022-05-29 11:02:17 +0300

    Installer language updated

--- upload/admin/language/en-gb/extension/installer.php
+++ upload/admin/language/en-gb/extension/installer.php
@@ -24,8 +24,10 @@ $_['help_upload']      = 'Requires a modification file with extension \'.ocmod.z
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify extensions!';
 $_['error_install']    = 'Extension installation taking place please wait a few seconds before trying to install!';
+$_['error_upload']     = 'File could not be uploaded!';
+$_['error_filetype']   = 'Invalid file type!';
+$_['error_file']       = 'File could not be found!';
 $_['error_unzip']      = 'Zip file could not be opened!';
-$_['error_file']       = 'Install file could not be found!';
 $_['error_directory']  = 'Install directory could not be found!';
 $_['error_code']       = 'Unique code is required for modification XML!';
 $_['error_xml']        = 'Modification %s is already being used!';
