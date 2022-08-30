d881a4deedd110f6ff9d86ad475d820c049d63a2
condor2 <condor20_05@yahoo.it>
2022-08-30 21:16:42 +0300

    Model API updated

--- upload/catalog/model/account/api.php
+++ upload/catalog/model/account/api.php
@@ -6,7 +6,7 @@ class ModelAccountApi extends Model {
 		return $query->row;
 	}
 
-	public function addApiSession(int $api_id, int $session_id, string $ip): int {
+	public function addApiSession($api_id, $session_id, $ip) {
 		$token = token(32);
 
 		$this->db->query("INSERT INTO `" . DB_PREFIX . "api_session` SET `api_id` = '" . (int)$api_id . "', `token` = '" . $this->db->escape($token) . "', `session_id` = '" . $this->db->escape($session_id) . "', `ip` = '" . $this->db->escape($ip) . "', `date_added` = NOW(), `date_modified` = NOW()");
