d9302688a4f7d4bfc711df1ae56bb57453e2bd9a
condor2 <condor20_05@yahoo.it>
2022-09-16 10:55:15 +0300

    Added Tax calculation improvment
    https://github.com/opencart/opencart/commit/1abe39b6368fb240b5be1460d9293572e16e67a7

--- upload/catalog/controller/extension/total/shipping.php
+++ upload/catalog/controller/extension/total/shipping.php
@@ -71,7 +71,7 @@ class ControllerExtensionTotalShipping extends Controller {
 			$json['error']['country'] = $this->language->get('error_country');
 		}
 
-		if (!isset($this->request->post['zone_id']) || $this->request->post['zone_id'] == '') {
+		if (!isset($this->request->post['zone_id']) || $this->request->post['zone_id'] == '' || !is_numeric($this->request->post['zone_id'])) {
 			$json['error']['zone'] = $this->language->get('error_zone');
 		}
 
@@ -84,8 +84,14 @@ class ControllerExtensionTotalShipping extends Controller {
 		}
 
 		if (!$json) {
+			$this->tax->unsetRates();
 			$this->tax->setShippingAddress($this->request->post['country_id'], $this->request->post['zone_id']);
-
+			if (isset($this->session->data['payment_address']['country_id']) && isset($this->session->data['payment_address']['zone_id'])) {
+				$this->tax->setPaymentAddress($this->session->data['payment_address']['country_id'], $this->session->data['payment_address']['zone_id']);
+			} elseif ($this->config->get('config_tax_default') == 'payment') {
+				$this->tax->setPaymentAddress($this->config->get('config_country_id'), $this->config->get('config_zone_id'));
+			}
+			$this->tax->setStoreAddress($this->config->get('config_country_id'), $this->config->get('config_zone_id'));
 			if ($country_info) {
 				$country = $country_info['name'];
 				$iso_code_2 = $country_info['iso_code_2'];
