9e745b8117fbf29c9e3d23496fea374959bad778
condor2 <condor20_05@yahoo.it>
2022-09-18 22:07:37 +0300

    Updated code for db library files as per Opencart 4
    https://github.com/opencart/opencart/tree/master/upload/system/library/db

--- upload/system/library/db/mysqli.php
+++ upload/system/library/db/mysqli.php
@@ -1,31 +1,32 @@
 <?php
 namespace DB;
 class MySQLi {
-	private $connection;
+	private object $connection;
 
-	public function __construct($hostname, $username, $password, $database, $port = '3306') {
-		try {
-			$mysqli = @new \MySQLi($hostname, $username, $password, $database, $port);
-		} catch (mysqli_sql_exception $e) {
-			throw new \Exception('Error: Could not make a database link using ' . $username . '@' . $hostname . '!');
+	public function __construct(string $hostname, string $username, string $password, string $database, string $port = '') {
+		if (!$port) {
+			$port = '3306';
 		}
 
-		if (!$mysqli->connect_errno) {
+		try {
+			$mysqli = @new \MySQLi($hostname, $username, $password, $database, $port);
+
 			$this->connection = $mysqli;
 			$this->connection->report_mode = MYSQLI_REPORT_ERROR;
 			$this->connection->set_charset('utf8');
 			$this->connection->query("SET SESSION sql_mode = 'NO_ZERO_IN_DATE,NO_ENGINE_SUBSTITUTION'");
-		} else {
+			$this->connection->query("SET FOREIGN_KEY_CHECKS = 0");
+		} catch (\mysqli_sql_exception $e) {
 			throw new \Exception('Error: Could not make a database link using ' . $username . '@' . $hostname . '!');
 		}
 	}
 
-	public function query($sql) {
-		$query = $this->connection->query($sql);
+	public function query(string $sql): bool|object {
+		try {
+			$query = $this->connection->query($sql);
 
-		if (!$this->connection->errno) {
 			if ($query instanceof \mysqli_result) {
-				$data = [];
+				$data = array();
 
 				while ($row = $query->fetch_assoc()) {
 					$data[] = $row;
@@ -44,24 +45,24 @@ class MySQLi {
 			} else {
 				return true;
 			}
-		} else {
-			throw new \Exception('Error: ' . $this->connection->error  . '<br />Error No: ' . $this->connection->errno . '<br />' . $sql);
+		} catch (\mysqli_sql_exception $e) {
+			throw new \Exception('Error: ' . $this->connection->error  . '<br/>Error No: ' . $this->connection->errno . '<br/>' . $sql);
 		}
 	}
 
-	public function escape($value) {
+	public function escape(string $value): string {
 		return $this->connection->real_escape_string($value);
 	}
-
-	public function countAffected() {
+	
+	public function countAffected(): int {
 		return $this->connection->affected_rows;
 	}
 
-	public function getLastId() {
+	public function getLastId(): int {
 		return $this->connection->insert_id;
 	}
-
-	public function isConnected() {
+	
+	public function isConnected(): bool {
 		if ($this->connection) {
 			return $this->connection->ping();
 		} else {
@@ -79,7 +80,7 @@ class MySQLi {
 		if ($this->connection) {
 			$this->connection->close();
 
-			$this->connection = '';
+			unset($this->connection);
 		}
 	}
 }
