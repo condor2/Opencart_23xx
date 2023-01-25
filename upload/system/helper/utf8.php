ea3a4822c61fc30e999e1b5c68a980bdef34f2b9
OSWorX <info@osworx.net>
2023-01-25 14:07:03 +0100

    Update utf8.php
    Fix if value is empty/null (can happen in validate() functions)

--- upload/system/helper/utf8.php
+++ upload/system/helper/utf8.php
@@ -3,7 +3,9 @@ if (extension_loaded('mbstring')) {
 	mb_internal_encoding('UTF-8');
 
 	function utf8_strlen($string) {
-		return mb_strlen($string);
+		if(!is_null($string)) {
+			return mb_strlen($string);
+		}
 	}
 
 	function utf8_strpos($string, $needle, $offset = 0) {
