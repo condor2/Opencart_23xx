67eab88f24ccbbb78452b2d1430c71cd3456ce2b
condor2 <condor20_05@yahoo.it>
2023-11-17 22:44:17 +0200

    Don't use strftime as deprecated in PHP 8.1
    https://github.com/opencart/opencart/commit/ec53c16d159dd13543350ee20376fbdc06b1456e

--- upload/catalog/controller/extension/payment/eway.php
+++ upload/catalog/controller/extension/payment/eway.php
@@ -26,7 +26,7 @@ class ControllerExtensionPaymentEway extends Controller {
 
 		for ($i = 1; $i <= 12; $i++) {
 			$data['months'][] = array(
-				'text' => sprintf('%02d', $i),
+				'text'  => sprintf('%02d', $i),
 				'value' => sprintf('%02d', $i)
 			);
 		}
@@ -37,8 +37,8 @@ class ControllerExtensionPaymentEway extends Controller {
 
 		for ($i = $today['year']; $i < $today['year'] + 11; $i++) {
 			$data['year_expire'][] = array(
-				'text' => strftime('%Y', mktime(0, 0, 0, 1, 1, $i)),
-				'value' => strftime('%Y', mktime(0, 0, 0, 1, 1, $i))
+				'text'  => sprintf('%02d', $i % 100),
+				'value' => sprintf('%04d', $i)
 			);
 		}
 
