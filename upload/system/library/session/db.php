b8e20ad9a324c2228d4f46d42aaedf180feef8cb
condor2 <condor20_05@yahoo.it>
2023-12-09 21:24:28 +0200

    Update db.php

--- upload/system/library/session/db.php
+++ upload/system/library/session/db.php
@@ -12,15 +12,15 @@ final class DB {
 	public $data = array();
 	public $expire = array();
 	public $db;
-	
+
 	public function __construct($registry) {
 		$this->db = $registry->get('db');
-		
+
 		register_shutdown_function('session_write_close');
-		
+
 		$this->expire = ini_get('session.gc_maxlifetime');
 	}
-	
+
 	public function open() {
 		if ($this->db){
 			return true;
@@ -28,36 +28,36 @@ final class DB {
 			return false;
 		}
 	}
-	
+
 	public function close() {
 		return true;
 	}
-	
+
 	public function read($session_id) {
 		$query = $this->db->query("SELECT `data` FROM `" . DB_PREFIX . "session` WHERE session_id = '" . $this->db->escape($session_id) . "' AND expire > " . (int)time());
-		
+
 		if ($query->num_rows) {
 			return $query->row['data'];
 		} else {
-			return false;
+			return array();
 		}
 	}
-	
+
 	public function write($session_id, $data) {
 		$this->db->query("REPLACE INTO SET `data` = '" . $this->db->escape($data) . "', expire = '" . $this->db->escape(date('Y-m-d H:i:s', time() + $this->expire)) . "' FROM `" . DB_PREFIX . "session` WHERE session_id = '" . $this->db->escape($session_id) . "' AND expire > " . (int)time());
-		
+
 		return true;
 	}
-	
+
 	public function destroy($session_id) {
 		$this->db->query("DELETE FROM `" . DB_PREFIX . "session` WHERE session_id = '" . $this->db->escape($session_id) . "'");
-		
+
 		return true;
 	}
-	
+
 	public function gc($expire) {
 		$this->db->query("DELETE FROM `" . DB_PREFIX . "session` WHERE expire < " . ((int)time() + $expire));
-		
+
 		return true;
 	}
 }
