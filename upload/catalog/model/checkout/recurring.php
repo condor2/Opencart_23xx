33c9c4bc85d38bde0e77047aeb7503c429277ecf
condor2 <condor20_05@yahoo.it>
2023-12-22 09:17:31 +0200

    Update recurring.php

--- upload/catalog/model/checkout/recurring.php
+++ upload/catalog/model/checkout/recurring.php
@@ -1,26 +1,26 @@
 <?php
 class ModelCheckoutRecurring extends Model {
-	public function create($recurring, $order_id, $description) {
+	public function create($recurring, $order_id, $description, $data) {
 
 		$this->db->query("INSERT INTO `" . DB_PREFIX . "order_recurring` SET 
 		`order_id` = '" . (int)$order_id . "', 
 		`date_added` = NOW(), 
 		`status` = 6, 
-		`product_id` = '" . (int)$item['product_id'] . "', 
-		`product_name` = '" . $this->db->escape((string)$item['name']) . "', 
-		`product_quantity` = '" . (int)$item['quantity'] . "', 
-		`recurring_id` = '" . (int)$item['recurring']['recurring_id'] . "', 
-		`recurring_name` = '" . $this->db->escape((string)$item['recurring']['name']) . "', 
+		`product_id` = '" . (int)$data['product_id'] . "', 
+		`product_name` = '" . $this->db->escape((string)$data['name']) . "', 
+		`product_quantity` = '" . (int)$data['quantity'] . "', 
+		`recurring_id` = '" . (int)$data['recurring']['recurring_id'] . "', 
+		`recurring_name` = '" . $this->db->escape((string)$data['recurring']['name']) . "', 
 		`recurring_description` = '" . $this->db->escape($description) . "', 
-		`recurring_frequency` = '" . $this->db->escape((string)$item['recurring']['frequency']) . "', 
-		`recurring_cycle` = '" . (int)$item['recurring']['cycle'] . "', 
-		`recurring_duration` = '" . (int)$item['recurring']['duration'] . "', 
-		`recurring_price` = '" . (float)$item['recurring']['price'] . "', 
-		`trial` = '" . (int)$item['recurring']['trial'] . "', 
-		`trial_frequency` = '" . $this->db->escape((string)$item['recurring']['trial_frequency']) . "', 
-		`trial_cycle` = '" . (int)$item['recurring']['trial_cycle'] . "', 
-		`trial_duration` = '" . (int)$item['recurring']['trial_duration'] . "', 
-		`trial_price` = '" . (float)$item['recurring']['trial_price'] . "', 
+		`recurring_frequency` = '" . $this->db->escape((string)$data['recurring']['frequency']) . "', 
+		`recurring_cycle` = '" . (int)$data['recurring']['cycle'] . "', 
+		`recurring_duration` = '" . (int)$data['recurring']['duration'] . "', 
+		`recurring_price` = '" . (float)$data['recurring']['price'] . "', 
+		`trial` = '" . (int)$data['recurring']['trial'] . "', 
+		`trial_frequency` = '" . $this->db->escape((string)$data['recurring']['trial_frequency']) . "', 
+		`trial_cycle` = '" . (int)$data['recurring']['trial_cycle'] . "', 
+		`trial_duration` = '" . (int)$data['recurring']['trial_duration'] . "', 
+		`trial_price` = '" . (float)$data['recurring']['trial_price'] . "', 
 		`reference` = ''");
 
 		return $this->db->getLastId();
