199e02baa88e96f999a674d067f2daea92336565
condor2 <condor20_05@yahoo.it>
2023-12-22 11:51:35 +0200

    Update proxy.php
    https://github.com/opencart/opencart-3/pull/177/files

--- upload/system/engine/proxy.php
+++ upload/system/engine/proxy.php
@@ -11,14 +11,15 @@
 * Proxy class
 */
 class Proxy extends \stdClass {
+	protected $data = array();
     /**
      * 
      *
      * @param	string	$key
      */	
 	public function __get($key) {
-		return $this->{$key};
-	}	
+		return $this->data[$key];
+	}
 
     /**
      * 
@@ -27,14 +28,14 @@ class Proxy extends \stdClass {
 	 * @param	string	$value
      */	
 	public function __set($key, $value) {
-		 $this->{$key} = $value;
+		 $this->data[$key] = $value;
 	}
-	
+
 	public function __call($key, $args) {
 		$arg_data = array();
-		
+
 		$args = func_get_args();
-		
+
 		foreach ($args as $arg) {
 			if ($arg instanceof Ref) {
 				$arg_data[] =& $arg->getRef();
@@ -42,12 +43,12 @@ class Proxy extends \stdClass {
 				$arg_data[] =& $arg;
 			}
 		}
-		
-		if (isset($this->{$key})) {		
-			return call_user_func_array($this->{$key}, $arg_data);	
+
+		if (isset($this->data[$key])) {
+			return call_user_func_array($this->data[$key], $arg_data);
 		} else {
 			$trace = debug_backtrace();
-			
+
 			exit('<b>Notice</b>:  Undefined property: Proxy::' . $key . ' in <b>' . $trace[1]['file'] . '</b> on line <b>' . $trace[1]['line'] . '</b>');
 		}
 	}
