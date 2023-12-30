c389b2797abf98ae2767a50cda2684e9c8f964fd
condor2 <condor20_05@yahoo.it>
2023-12-30 16:40:29 +0200

    public function info update in order account controller
    https://github.com/opencart/opencart/blob/master/upload/catalog/controller/account/order.php#L405-L409

--- upload/catalog/controller/account/order.php
+++ upload/catalog/controller/account/order.php
@@ -377,52 +377,11 @@ class ControllerAccountOrder extends Controller {
 			$data['header'] = $this->load->controller('common/header');
 
 			$this->response->setOutput($this->load->view('account/order_info', $data));
+
+			return null;
 		} else {
-			$this->document->setTitle($this->language->get('text_order'));
-
-			$data['heading_title'] = $this->language->get('text_order');
-
-			$data['text_error'] = $this->language->get('text_error');
-
-			$data['button_continue'] = $this->language->get('button_continue');
-
-			$data['breadcrumbs'] = [];
-
-			$data['breadcrumbs'][] = [
-				'text' => $this->language->get('text_home'),
-				'href' => $this->url->link('common/home')
-			];
-
-			$data['breadcrumbs'][] = [
-				'text' => $this->language->get('text_account'),
-				'href' => $this->url->link('account/account', '', true)
-			];
-
-			$data['breadcrumbs'][] = [
-				'text' => $this->language->get('heading_title'),
-				'href' => $this->url->link('account/order', '', true)
-			];
-
-			$data['breadcrumbs'][] = [
-				'text' => $this->language->get('text_order'),
-				'href' => $this->url->link('account/order/info', 'order_id=' . $order_id, true)
-			];
-
-			$data['continue'] = $this->url->link('account/order', '', true);
-
-			$data['column_left'] = $this->load->controller('common/column_left');
-			$data['column_right'] = $this->load->controller('common/column_right');
-			$data['content_top'] = $this->load->controller('common/content_top');
-			$data['content_bottom'] = $this->load->controller('common/content_bottom');
-			$data['footer'] = $this->load->controller('common/footer');
-			$data['header'] = $this->load->controller('common/header');
-
-			$this->response->setOutput($this->load->view('error/not_found', $data));
-		} else {
-			return new \Action('error/not_found');
+			return new \Opencart\System\Engine\Action('error/not_found');
 		}
-
-		return null;
 	}
 
 	public function reorder(): void {
