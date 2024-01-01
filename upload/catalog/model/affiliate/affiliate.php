a9ebfb17bc8cfd01575a290c212055efc8f3134b
condor2 <condor20_05@yahoo.it>
2024-01-01 12:10:59 +0200

    affiliate update

--- upload/catalog/model/affiliate/affiliate.php
+++ upload/catalog/model/affiliate/affiliate.php
@@ -115,7 +115,7 @@ class ModelAffiliateAffiliate extends Model {
 		return (int)$query->row['total'];
 	}
 
-	public function addTransaction(int $affiliate_id, int $amount = 0, int $order_id = 0): int {
+	public function addTransaction(int $affiliate_id, int $amount = 0, int $order_id = 0) {
 		$affiliate_info = $this->getAffiliate($affiliate_id);
 
 		if ($affiliate_info) {
