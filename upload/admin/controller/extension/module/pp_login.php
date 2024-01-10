24b8dcc638a3d744ea5bffcd1d38c45c0cb6a260
condor2 <condor20_05@yahoo.it>
2024-01-10 12:52:16 +0200

    Update pp_login.php

--- upload/admin/controller/extension/module/pp_login.php
+++ upload/admin/controller/extension/module/pp_login.php
@@ -2,7 +2,7 @@
 class ControllerExtensionModulePPLogin extends Controller {
 	private $error = [];
 
-	public function index() {
+	public function index(): void {
 		$this->load->language('extension/module/pp_login');
 
 		$this->load->model('setting/setting');
