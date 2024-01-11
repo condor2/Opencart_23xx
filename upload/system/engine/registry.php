922428b1e11d008ed1a9dee633f0d1bfb832330c
Anders Jenbo <anders@jenbo.dk>
2024-01-11 22:05:12 +0100

    Document basic object relations in a way that can be parsed by PHPStan

--- upload/system/engine/registry.php
+++ upload/system/engine/registry.php
@@ -11,16 +11,44 @@
 
 /**
  * Registry class
- */
-
-/**
- * Class Registry
  *
- * @property \Admin\Controller\Catalog\Attribute $load
+ * @property Cache          $cache
+ * @property Cart\Affiliate $affiliate
+ * @property Cart\Cart      $cart
+ * @property Cart\Currency  $currency
+ * @property Cart\Customer  $customer
+ * @property Cart\Length    $length
+ * @property Cart\Tax       $tax
+ * @property ?Cart\User     $user
+ * @property Cart\Weight    $weight
+ * @property Config         $config
+ * @property DB             $db
+ * @property Document       $document
+ * @property Encryption     $encryption
+ * @property Event          $event
+ * @property Language       $language
+ * @property Loader         $load
+ * @property Log            $log
+ * @property Request        $request
+ * @property Response       $response
+ * @property Session        $session
+ * @property Url            $url
  */
-
 class Registry {
 	private $data = [];
+
+	/**
+	 * __get
+	 *
+	 * https://www.php.net/manual/en/language.oop5.overloading.php#object.get
+	 *
+	 * @param string $key
+	 *
+	 * @return ?object
+	 */
+	public function __get(string $key): ?object {
+		return $this->get($key);
+	}
 
 	/**
 	 * Get
