5b5d294c552908ae61c5715fb79f0d1f009fc42b
condor2 <condor20_05@yahoo.it>
2024-02-27 15:04:16 +0200

    Added missed paypal_oder_data

--- upload/catalog/controller/extension/payment/paypal.php
+++ upload/catalog/controller/extension/payment/paypal.php
@@ -3330,6 +3330,7 @@ class ControllerExtensionPaymentPayPal extends Controller {
 									$order_status_id = 0;
 									$transaction_status = '';
 									$payment_method = 'card';
+									$paypal_order_data = [];
 
 									if (!$this->cart->hasShipping()) {
 										$seller_protection_status = 'NOT_ELIGIBLE';
