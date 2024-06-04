f1583b83a4c433bebfc1b16f582dbac022117c97
condor2 <condor20_05@yahoo.it>
2024-06-04 10:46:27 +0300

    Added Paypal Order Info Heading title Bugfix
    https://github.com/opencart/opencart/commit/4d0bccb8a518997fee3968c15cd6107e1c7d399f

--- upload/admin/controller/extension/payment/paypal.php
+++ upload/admin/controller/extension/payment/paypal.php
@@ -2722,14 +2722,14 @@ class ControllerExtensionPaymentPayPal extends Controller {
 
 	public function order_info_before($route, &$data) {
 		if ($this->config->get('paypal_status') && !empty($this->request->get['order_id'])) {
-			$this->load->language('extension/payment/paypal');
+			$this->load->language('extension/payment/paypal','extension_payment_paypal');
 
 			$content = $this->getPaymentDetails((int)$this->request->get['order_id']);
 
 			if ($content) {
 				$data['tabs'][] = [
 					'code'    => 'paypal',
-					'title'   => $this->language->get('heading_title_main'),
+					'title'   => $this->language->get('extension_payment_paypal')->get('heading_title_main'),
 					'content' => $content
 				];
 			}
