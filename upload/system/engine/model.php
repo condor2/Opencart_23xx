c2dacb5ab43179d929f82b646638f810f5c02c82
condor2 <condor20_05@yahoo.it>
2024-06-20 14:15:09 +0300

    reverted code model engine

--- upload/system/engine/model.php
+++ upload/system/engine/model.php
@@ -36,12 +36,8 @@ class Model {
 	 *
 	 * @return object
 	 */
-	public function __get(string $key): object {
-		if ($this->registry->has($key)) {
-			return $this->registry->get($key);
-		} else {
-			throw new \Exception('Error: Could not call registry key ' . $key . '!');
-		}
+	public function __get($key) {
+		return $this->registry->get($key);
 	}
 
 	/**
