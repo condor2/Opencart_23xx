3ec1e488af34c45d26d1ddf786312b6554b7e31a
condor2 <condor20_05@yahoo.it>
2024-12-21 08:56:57 +0200

    Changed php-cs-fixer,phpstan like in oc 4

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * This document has been generated with
- * https://mlocati.github.io/php-cs-fixer-configurator/#version:3.59.3|configurator
+ * https://mlocati.github.io/php-cs-fixer-configurator/#version:3.65|configurator
  * you can change this configuration by importing this file.
  */
 $config = new PhpCsFixer\Config();
